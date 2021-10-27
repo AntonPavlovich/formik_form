7983b5972f552eeb9fe35aa62ab5b589e885e863
Anton <antopavlu@gmail.com>
2021-10-27 12:36:15 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 
@@ -8,8 +13,9 @@ const App = () => {
     <>
       <Router>
         <Switch>
-          <Route exact path="/" component={LoginPage}/>
-          <Route path='/sign_up' component={SignUpPage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/sign_up" component={SignUpPage} />
+          <Redirect from='/' to='login' />
         </Switch>
       </Router>
     </>
