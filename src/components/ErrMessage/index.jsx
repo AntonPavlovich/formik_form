33818265dbd610c24d4c14da1465d4695d14d70c
Anton <antopavlu@gmail.com>
2021-10-27 17:37:15 +0300

    add input reaction to error

--- src/components/ErrMessage/index.jsx
+++ src/components/ErrMessage/index.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './ErrMessage.module.scss'
 
 const ErrMessage = (props) => {
-  const {message} = props;
+  const {message,setIsError} = props;
+
+  useEffect(() => {
+    setIsError(true)
+
+    return () => setIsError(false)
+  },[])
 
   return (
     <p className={styles.errMessage}>
--- src/components/FieldWithValidation/index.jsx
+++ src/components/FieldWithValidation/index.jsx
@@ -1,16 +1,26 @@
-import { ErrorMessage, Field } from 'formik';
-import React from 'react';
-import ErrMessage from '../ErrMessage';
-import styles from './FieldWithValidation.module.scss'
+import { ErrorMessage, Field } from "formik";
+import React, { useState } from "react";
+import ErrMessage from "../ErrMessage";
+import styles from "./FieldWithValidation.module.scss";
+import cx from "classnames";
 
 const FieldWithValidation = (props) => {
-  const {name,placeholder} = props;
+  const { name, placeholder } = props;
+
+  const [isError, setIsError] = useState(false);
+
+  const fieldClasses = cx(styles.input, {
+    [styles.errStyle]: isError,
+  });
   return (
     <div className={styles.inputWrapper}>
-      <Field name={name} placeholder={placeholder} className={styles.input}/>
-      <ErrorMessage name={name} render={msg => <ErrMessage message={msg}/>}/>
+      <Field name={name} placeholder={placeholder} className={fieldClasses} />
+      <ErrorMessage
+        name={name}
+        render={(msg) => <ErrMessage setIsError={setIsError} message={msg} />}
+      />
     </div>
   );
-}
+};
 
 export default FieldWithValidation;
