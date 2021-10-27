a87eeaf2d93ffc053a5be5d0603aedb883d5a5d1
Anton <antopavlu@gmail.com>
2021-10-27 17:08:54 +0300

    add message for errors

---
+++ src/schemas/schemasMessages.js
@@ -0,0 +1,2 @@
+export const forRequired = 'Its a required field*';
+export const forRadioButtons = 'Please choose your plan';
--- src/schemas/signUpSchema.js
+++ src/schemas/signUpSchema.js
@@ -1,12 +1,14 @@
 import * as yup from "yup";
+import * as messages from './schemasMessages'
 
 const signUpSchema = yup.object().shape({
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
-  displayName: yup.string().required(),
-  email: yup.string().required(),
-  password: yup.string().required(),
-  joinAs: yup.string().required(),
+  firstName: yup.string().required(messages.forRequired),
+  lastName: yup.string().required(messages.forRequired),
+  displayName: yup.string().required(messages.forRequired),
+  email: yup.string().required(messages.forRequired),
+  password: yup.string().required(messages.forRequired),
+  confirmPassword : yup.string().required(messages.forRequired),
+  joinAs: yup.string().required(messages.forRadioButtons),
 });
 
 export default signUpSchema;
