42e3a1fda62ecbd075e138b07f4d7c94ac50ac2b
Anton <antopavlu@gmail.com>
2021-10-27 11:55:51 +0300

    add header to my pages

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../../components/Header';
 
 const LoginPage = () => {
   return (
     <div>
-      LOGIN PAGE
+      <Header btnLinkTo='/sign_up' btnText='Signup'/>
+      
     </div>
   );
 }
--- src/pages/SignUpPage/index.jsx
+++ src/pages/SignUpPage/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../../components/Header';
 
 const SignUpPage = () => {
   return (
     <div>
-        SIGN UP PAGE
+        <Header btnLinkTo='/login' btnText='Login'/>
     </div>
   );
 }
