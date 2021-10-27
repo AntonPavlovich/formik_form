bf429c5d8cb3103ae6705e284b4bdb8ad9557ea1
Anton <antopavlu@gmail.com>
2021-10-27 14:43:36 +0300

    finish joinAsButtons component

--- src/components/JoinAsButtons/index.jsx
+++ src/components/JoinAsButtons/index.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import { Field } from "formik";
+import styles from "./JoinAsButtons.module.scss";
 
-const JoinAsButtons = () => {
+const JoinAsButtons = (props) => {
+  const { name } = props;
   return (
-    <>
-      <Field type="radio" name="joinAs" value="Buyer" />
-      <Field type="radio" name="joinAs" value="Marketplace seller" />
-    </>
+    <div className={styles.joinAsButtons}>
+      <label className={styles.labelWrapper}>
+        <Field type="radio" name={name} value="Buyer" />
+        <div className={styles.textWrapper}>
+          <h2>Join as a buyer</h2>
+          <p>
+            I am looking for a Name, Logo or Tagline for my business, brand or
+            product
+          </p>
+        </div>
+      </label>
+      <label className={styles.labelWrapper}>
+        <Field type="radio" name={name} value="Seller" />
+        <div className={styles.textWrapper}>
+          <h2>Join as a Creative or Marketplace Seller</h2>
+          <p>
+            I plan to submit name ideas, Logo designs or sell names in Domain
+            Marketplace
+          </p>
+        </div>
+      </label>
+    </div>
   );
 };
 
