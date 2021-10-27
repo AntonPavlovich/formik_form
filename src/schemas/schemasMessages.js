a3c1d8be17b15acedd765453408c3bebae908998
Anton <antopavlu@gmail.com>
2021-10-27 19:40:19 +0300

    add schema validtion

--- src/schemas/schemasMessages.js
+++ src/schemas/schemasMessages.js
@@ -1,2 +1,5 @@
-export const forRequired = 'Its a required field*';
+export const forRequired = 'Its a required field';
 export const forRadioButtons = 'Please choose your plan';
+export const emailValidation = 'Format: "example@mail.com"';
+export const passwordValidation = 'Min 8 symbols,one uppercase,number and symbol';
+export const passwordConfirmation = 'Password don`t matches'
--- src/schemas/signUpSchema.js
+++ src/schemas/signUpSchema.js
@@ -1,14 +1,36 @@
 import * as yup from "yup";
-import * as messages from './schemasMessages'
+import * as messages from "./schemasMessages";
 
 const signUpSchema = yup.object().shape({
-  firstName: yup.string().required(messages.forRequired),
-  lastName: yup.string().required(messages.forRequired),
-  displayName: yup.string().required(messages.forRequired),
-  email: yup.string().required(messages.forRequired),
-  password: yup.string().required(messages.forRequired),
-  confirmPassword : yup.string().required(messages.forRequired),
+  firstName: yup.string().max(100).required(messages.forRequired),
+
+  lastName: yup.string().max(100).required(messages.forRequired),
+
+  displayName: yup.string().max(100).required(messages.forRequired),
+
+  email: yup
+    .string()
+    .matches(
+      /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+      messages.emailValidation
+    )
+    .required(messages.forRequired),
+
+  password: yup
+    .string()
+    .matches(
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
+      messages.passwordValidation
+    )
+    .required(messages.forRequired),
+
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match"),
+
   joinAs: yup.string().required(messages.forRadioButtons),
+
+  allowOffer: yup.boolean(),
 });
 
 export default signUpSchema;
