106d8116e5ef3bcd8fdb42c6695a4e5c4264264f
Anton <antopavlu@gmail.com>
2021-10-28 19:32:01 +0300

    add input styles

--- src/components/FieldWithValidation/index.jsx
+++ src/components/FieldWithValidation/index.jsx
@@ -5,19 +5,23 @@ import styles from "./FieldWithValidation.module.scss";
 import cx from "classnames";
 
 const FieldWithValidation = (props) => {
+  const { name, placeholder,inRow ,type} = props;
   const [,meta] = useField(props);
 
-  const { name, placeholder } = props;
-
   const [isError, setIsError] = useState(false);
+
+  const wrapperClasses = cx({
+    [styles.inputWrapper] : !inRow,
+    [styles.inputWrapperLong] : inRow
+  })
 
   const fieldClasses = cx(styles.input, {
     [styles.errStyle]: isError,
     [styles.correct] : !meta.error && meta.touched  
   });
   return (
-    <div className={styles.inputWrapper}>
-      <Field name={name} placeholder={placeholder} className={fieldClasses} />
+    <div className={wrapperClasses}>
+      <Field type={type} name={name} placeholder={placeholder} className={fieldClasses} />
       <ErrorMessage
         name={name}
         render={(msg) => <ErrMessage setIsError={setIsError} message={msg} />}
